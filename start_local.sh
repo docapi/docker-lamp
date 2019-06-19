c88354792c89d870f756268781da61c1c39cc261
Alex <api@active-value.de>
2019-06-19 15:54:48 +0200

    improved start script

--- start_local.sh
+++ start_local.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+DOMAIN=myweb.test
+
 docker-compose up -d
 
-echo "Webserver started at http://myweb.test"
+if grep -q $DOMAIN /etc/hosts; then
+    echo "Host entry exists"
+else
+    echo "127.0.0.1 $DOMAIN"  | sudo tee -a /etc/hosts > /dev/null
+    echo "Added new host entry"
+fi
+
+echo "Webserver started at http://$DOMAIN"
